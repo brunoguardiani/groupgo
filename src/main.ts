f483d75e20f935e7149a9a02e6149426f4559633
Bruno <brunodipaolo13@gmail.com>
2025-02-23 18:47:37 -0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',  // Allow frontend running on this URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept, Authorization',  // Allowed headers
+    credentials: true,  // Allow cookies or authentication tokens
+  });
+
   await app.listen(3000);
 }
 bootstrap();
