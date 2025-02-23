2e223f2b8a55e5e7e61e89310358042eb58b0848
Bruno <brunodipaolo13@gmail.com>
2025-02-23 16:59:42 -0300

    Back to postgres

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,22 +2,13 @@
 // src/config/database.config.ts
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-
-// POSTGRES CONFIG
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST || 'postgres',
-//   port: +process.env.DB_PORT || 5432,
-//   username: process.env.DB_USERNAME || 'postgres',
-//   password: process.env.DB_PASSWORD || 'postgres',
-//   database: process.env.DB_NAME || 'postgres',
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: true,
-// };
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type:'sqlite',
-  database: '/tmp/database.sqlite',
+  type: 'postgres',
+  host: process.env.DB_HOST || 'postgres',
+  port: +process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true
-}
+  synchronize: true,
+};
